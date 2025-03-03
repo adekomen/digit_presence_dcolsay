e80342d89ecd3dea6a9230756e570ce14e42154b
adekomen <k.francoisadesu@gmail.com>
2025-03-04 00:29:00 +0100

    Restructuration du code et modification de la page d'accueil

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import '../widgets/qr_scanner.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Digit Presence DCOLSAY'),
+        backgroundColor: Colors.blue,
+        elevation: 10,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blue.shade400,
+              Colors.red.shade400,
+              Colors.yellow.shade600,
+              Colors.green.shade400,
+            ],
+            stops: const [
+              0.2,
+              0.5,
+              0.7,
+              1.0
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/dcolsay_img.jpg',
+                height: 150,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Bienvenue sur Digit Presence',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Scannez votre code QR pour enregistrer votre présence',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white70,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const QRScanner(),
+                  ));
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.blue,
+                  backgroundColor: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                ),
+                child: const Text(
+                  'SCANNER ICI',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
